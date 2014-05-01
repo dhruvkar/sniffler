e8a193536f60508b534f71aa701d37134acc2a59
dhruvkar <kar.dhruv@gmail.com>
2014-05-01 21:34:12 +0000

    Corrections made to sniffles.py, so now it actually works

--- sniffles.py
+++ sniffles.py
@@ -8,10 +8,26 @@ import os
 import time
 import calendar
 import signal
-import click
+import sys
+
+def checksum(msg):
+    s = 0
+     
+    # loop taking 2 characters at a time
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8 )
+        s = s + w
+     
+    s = (s>>16) + (s & 0xffff);
+    s = s + (s >> 16);
+     
+    #complement and mask to 4 byte short
+    s = ~s & 0xffff
+     
+    return s
 
 def sniff(TIMEOUT):
-	# Create a socket to use for sniffing. Allow it to receive packets with a max length of 2048 bytes.
+    # Create a socket to use for sniffing. Allow it to receive packets with a max length of 2048 bytes.
     r = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
 
     start = time.time()
@@ -45,8 +61,8 @@ def sniff(TIMEOUT):
 
 
 def inject():
-	s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
-	s.bind(("wlan0", socket.htons(0x0003)))
+	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+	#s.bind(("eth0", socket.htons(0x0003)))
 
 	source_ip = raw_input("Enter a Source IP address: ")
 	dest_ip = raw_input("Enter an Destination IP address: ")
@@ -55,23 +71,25 @@ def inject():
 	ip_ihl = 5
 	ip_ver = 4
 	ip_tos = 0
-	ip_tot_len = 0  # kernel will fill the correct total length
+	ip_tot_len = 0  
 	ip_id = 54321   #Id of this packet
 	ip_frag_off = 0
 	ip_ttl = 255
 	ip_proto = socket.IPPROTO_TCP
-	ip_check = 0    # kernel will fill the correct checksum
-	ip_saddr = socket.inet_aton ( source_ip )   #Spoof the source ip address if you want to
-	ip_daddr = socket.inet_aton ( dest_ip )
+	ip_check = 0   
+	ip_saddr = socket.inet_aton(source_ip)   #Spoof the source ip address if you want to
+	ip_daddr = socket.inet_aton(dest_ip)
 	 
-	ip_ihl_ver = (version << 4) + ihl
+	ip_ihl_ver = (ip_ver << 4) + ip_ihl
 	 
 	# the ! in the pack format string means network order
-	ip_header = pack('!BBHHHBBH4s4s', ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
+	ip_header = struct.pack('!BBHHHBBH4s4s', ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
 
 	# tcp header fields
-	tcp_source = raw_input("Enter a Source Port: ")   		#source port
+	tcp_source = raw_input("Enter a Source Port: ")   	#source port
+	tcp_src = int(tcp_source)
 	tcp_dest = raw_input("Enter a Destination Port: ")   	#destination port
+	tcp_dst = int(tcp_dest)
 	tcp_seq = 454
 	tcp_ack_seq = 0
 	tcp_doff = 5    		#4 bit field, size of tcp header, 5 * 4 = 20 bytes
@@ -82,7 +100,7 @@ def inject():
 	tcp_psh = 0
 	tcp_ack = 0
 	tcp_urg = 0
-	tcp_window = socket.htons(5840)    #   maximum allowed window size
+	tcp_window = socket.htons(5840)    # maximum window size
 	tcp_check = 0
 	tcp_urg_ptr = 0
 	 
@@ -90,20 +108,22 @@ def inject():
 	tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack << 4) + (tcp_urg << 5)
 	 
 	# First packing of the TCP header. 
-	tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window, tcp_check, tcp_urg_ptr)
+	tcp_header = struct.pack('!HHLLBBHHH' , tcp_src, tcp_dst, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags, tcp_window, tcp_check, tcp_urg_ptr)
+
+	user_data = "Hello, how are you" #raw_input("Enter data you would like to send: ")	
 	
 	# Format the addresses/parts of the header for the final pack
-	source_address = socket.inet_aton( source_ip )
+	source_address = socket.inet_aton(source_ip)
 	dest_address = socket.inet_aton(dest_ip)
 	placeholder = 0
 	protocol = socket.IPPROTO_TCP
 	tcp_length = len(tcp_header) + len(user_data)
 
-	psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
+	psh = struct.pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
 	psh = psh + tcp_header + user_data
 	tcp_check = checksum(psh)
 
-	tcp_header = pack('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window) + pack('H' , tcp_check) + pack('!H' , tcp_urg_ptr)
+	tcp_header = struct.pack('!HHLLBBH' , tcp_src, tcp_dst, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window) + struct.pack('H' , tcp_check) + struct.pack('!H' , tcp_urg_ptr)
 	packet = ip_header + tcp_header + user_data
 
 	s.sendto(packet, (dest_ip, 0))
